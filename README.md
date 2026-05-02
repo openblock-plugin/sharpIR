294254ce9bb4cc3003c345119c8f0d505b23d710
Arthur Zheng <arthurzheng150@gmail.com>
2026-05-02 13:22:10 +0800

    init

---
+++ README.md
@@ -0,0 +1,14 @@
+# Sharp IR Sensor
+
+![Version](https://img.shields.io/github/v/tag/openblock-plugin/sharpIR?label=version)
+![License](https://img.shields.io/badge/license-MIT-green.svg)
+![Type](https://img.shields.io/badge/type-extension-orange.svg)
+![Tags](https://img.shields.io/badge/tags-sensor-lightgrey.svg)
+
+Infrared distance sensor based on the principle of triangle ranging.
+
+## Documentation
+
+For detailed usage instructions, please visit:
+
+**[OpenBlock Wiki](https://wiki.openblock.cc)**
---
+++ src/translations.js
@@ -0,0 +1,67 @@
+/* eslint-disable quote-props */
+/* eslint-disable max-len */
+/**
+ * Translation file for this resouce.
+ *
+ * IMPORTANT:
+ * - The "en" (English) section is automatically generated from source files.
+ *   Do NOT modify the "en" section manually.
+ * - Other language sections (e.g., "zh-cn", "zh-tw", "ja", etc.) should be
+ *   manually translated by you.
+ * - When you run the extraction script again, only the "en" section will be
+ *   updated. Your manual translations in other languages will be preserved.
+ *
+ * Structure:
+ * - interface: translations for name/description (used by GUI formatMessage)
+ * - extensions: translations for extension blocks (used by VM formatMessage)
+ * - blocks: translations for Blockly blocks (used by Blockly.Msg)
+ */
+
+export default {
+    'interface': {
+        'en': {
+            'sharpIR.name': 'Sharp IR Sensor',
+            'sharpIR.description': 'Infrared distance sensor based on the principle of triangle ranging.'
+        },
+        'ru': {
+            'sharpIR.name': 'Sharp IR Sensor',
+            'sharpIR.description': 'Infrared distance sensor based on the principle of triangle ranging.'
+        },
+        'zh-cn': {
+            'sharpIR.name': 'Sharp IR Sensor',
+            'sharpIR.description': 'Infrared distance sensor based on the principle of triangle ranging.'
+        },
+        'zh-tw': {
+            'sharpIR.name': 'Sharp IR Sensor',
+            'sharpIR.description': 'Infrared distance sensor based on the principle of triangle ranging.'
+        }
+    },
+    'extensions': {
+        'en': {},
+        'ru': {},
+        'zh-cn': {},
+        'zh-tw': {}
+    },
+    'blocks': {
+        'en': {
+            'SHARPIR_CATEGORY': 'Sharp IR',
+            'SHARPIR_INIT': 'init sharp IR sensor %1 pin %2 model %3',
+            'SHARPIR_READDISTANCE': 'sharp IR sensor %1 read distance'
+        },
+        'ru': {
+            'SHARPIR_CATEGORY': 'Sharp IR',
+            'SHARPIR_INIT': 'init sharp IR sensor %1 pin %2 model %3',
+            'SHARPIR_READDISTANCE': 'sharp IR sensor %1 read distance'
+        },
+        'zh-cn': {
+            'SHARPIR_CATEGORY': 'Sharp IR',
+            'SHARPIR_INIT': 'init sharp IR sensor %1 pin %2 model %3',
+            'SHARPIR_READDISTANCE': 'sharp IR sensor %1 read distance'
+        },
+        'zh-tw': {
+            'SHARPIR_CATEGORY': 'Sharp IR',
+            'SHARPIR_INIT': 'init sharp IR sensor %1 pin %2 model %3',
+            'SHARPIR_READDISTANCE': 'sharp IR sensor %1 read distance'
+        }
+    }
+};
